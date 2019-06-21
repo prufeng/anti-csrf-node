926814cbe05ec31556c9482374394e2bfda56779
Pan Rufeng <xingzhikong@126.com>
2019-06-21 16:35:14 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user', { title: 'User', users: users });
+});
+
+/* Add user */
+router.post('/', (req, res, next) => {
+  users.push(req.body.userName);
+  res.render('user', { title: 'User', users: users });
 });
 
 module.exports = router;
